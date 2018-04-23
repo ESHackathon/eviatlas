5df03ac0c30d474efecc85449c6e119a1e000f84
Sanita Dhaubanjar <sdhauban@Sanitas-MacBook-Pro.local>
2018-04-23 19:53:57 +0545

    Created code issue #2

---
+++ exploration/heatmap00.R
@@ -0,0 +1,14 @@
+#' Simple R code to create heatmap from dataset
+
+# Heat Map from SM database
+# Created By    : Sanita Dhaubanjar on 23 April 2018
+# Created For	  : ES Hackathon 2018
+
+#=========================
+
+# Import data -------
+
+
+# Select vars to filter by -------
+
+# Create heat map -------
