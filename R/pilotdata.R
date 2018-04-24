806e4cebfc2d7da2c1b9097cab49288541bbaea6
softloud <charlestigray@gmail.com>
2018-04-24 11:39:33 +0200

    Pilot data has been somewhat documented. We don't really need all that much detail, as we'll be replacing it with an open source dataset #3. :notes:

---
+++ R/pilotdata.R
@@ -0,0 +1,9 @@
+#' Development dataset
+#'
+#' This hasn't been published yet, so we'll need to source a different dataset for publication of the `eviatlas` package.
+#'
+#' @format A dataframe.
+#' \describe{
+#'   \item{variables}{We don't need to fully document this dataset as this is a placeholder dataset.}
+#' }
+"pilotdata"
