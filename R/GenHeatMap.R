e704908aa467d46ee460ea8e361c048fd0051315
Andrew Feierman <andrew.feierman@gmail.com>
2018-04-25 05:42:05 -0400

    package stuff is urgh

--- R/GenHeatMap.R
+++ R/GenHeatMap.R
@@ -4,7 +4,7 @@
 #' Created For	  : ES Hackathon 2018
 #'
 #' @param idata Input data frame
-#' @param selcols List of strings with column names
+#' @param selcols Vector of strings with column names
 #' @return Returns a heatmap object showing number of literature under different categories in user specified \code{selcols}
 #'
 #' @author Sanita Dhaubanjar 24 April 2018
@@ -16,31 +16,35 @@
 GenHeatMap = function(idata, selcols, verbose = FALSE){
   library(tidyverse)
   library(ggplot2)
-  library(reshape2)
 
   # Convert columns to factors to allow for categorical classification for both numeric and character data -------
   tmp <- as.data.frame(sapply(idata[selcols], function(x) as.factor(x)))
 
   # Count combination of vars --------
-  seldata<- tmp %>%
-    add_count_(selcols) %>%
-    complete_(selcols, fill = list(n=0)) %>% distinct()
+  seldata <- table(tmp)
+  seldata <- melt(seldata)
+  ind <- which(colnames(seldata) %in% "value")
+  colnames(seldata)[ind] <- "n"
 
   if (verbose) {
     message("GenHeatMap: Counting of var combination completed.")
   }
 
   # Plot Heatmap ------
-  #ggplot(seldata, aes(x=Extracted.by,y=Appraised.by, fill=n))+geom_raster()
-  heatmp<- ggplot(seldata, aes_string(x=colnames(seldata[1]),y=colnames(seldata[2]), fill=colnames(seldata[3]))) +
-    geom_raster(alpha=0.9) +
-    geom_text(aes_string( label = colnames(seldata[3]))) + #fill = colnames(seldata[3]),
-    theme(axis.line = element_line(colour = "black"),panel.background = element_blank()) +
-    scale_fill_gradient(low = "white", high = "light blue",name = "No of studies")
-  heatmp
+  heatmp <- ggplot2::ggplot(seldata,
+                            ggplot2::aes_string(x=colnames(seldata[1]),
+                                                y=colnames(seldata[2]),
+                                                fill=colnames(seldata[3]))) +
+    ggplot2::geom_raster(alpha=0.9) +
+    ggplot2::geom_text(ggplot2::aes_string(label = colnames(seldata[3]))) +
+    theme(axis.line = ggplot2::element_line(colour = "black"),
+          panel.background = ggplot2::element_blank()) +
+    ggplot2::scale_fill_gradient(low = "white", high = "light blue",
+                        name = "No of studies")
   if (verbose) {
     message("GenHeatMap: Heatmap created!")
   }
   return(heatmp)
+
 }
 
