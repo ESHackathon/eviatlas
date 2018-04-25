7c15b8a1e5566ec9834643986d0e40325a005cd2
ëzz-uh <ezgitanriver@gmail.com>
2018-04-25 15:24:42 +0200

    Update

--- R/GenTimeTrend.R
+++ R/GenTimeTrend.R
@@ -3,45 +3,46 @@
 #' Created For	  : ES Hackathon 2018
 #' @param idata Input data frame
 #' @param year_column List of strings with column names
+#' @param verbose Realtime commenting
 #' @return Returns a timetrend object showing number of literature under different categories in user specified \code{year_column}
 #'
 #' @author Ezgi Tanriver-Ayder 24 April 2018
 #'
 #' @keywords SystematicReview,
-#' @export year_column="Year", verbose = FALSE){
+#' @export
+#'
 GenTimeTrend = function(idata, year_column = NULL, verbose = FALSE){
 
   # Check if Year column exists ------
-  if(any(colnames(idata) %in% year_column))
+  if(any(colnames(idata) %in% year_column)) {
     message("GenTimeTrend: Year column found")
 
+  tmp <- as.data.frame(sapply(idata[year_column], function(x) as.factor(x)))
 
-  # Count combination of vars --------
-    seldata<- idata[year_column] %>%
-      add_count_(year_column) %>%
-      complete_(year_column, fill = list(n=0)) %>% distinct()
+    # Count combination of vars --------
+    seldata <- table(tmp)
+    seldata <- reshape2::melt(seldata)
 
-  # Plot Heatmap ------
+    # Plot Heatmap ------
     # Plot bar chart
     iyears<- min(seldata$Year):max(seldata$Year)
-    timemp<- ggplot(seldata, aes_string(x=colnames(seldata[1]),y=colnames(seldata[2]))) +
-      geom_bar(alpha=0.9, stat="identity",fill="light blue") +
-      #geom_text(aes( label = colnames(seldata[2]))) + #fill = colnames(seldata[3]),
-      scale_x_continuous(name="Year", breaks = iyears)+
-      labs(y="No of studies") + ggtitle("Distribution of studies over time") +
-      theme_bw() +
-      theme(axis.line = element_line(colour = "black"),
-            panel.background = element_blank(),
-            plot.title = element_text(hjust = .5))
-    timemp
-    if (verbose) {
-      message("GenTimeTrend: Time trend plot created!")
-    }
-    timemp
+    timemp<- ggplot2::ggplot(seldata,
+                             ggplot2::aes_string(x=colnames(seldata[1]),
+                                                 y=colnames(seldata[2]))) +
+      ggplot2::geom_bar(alpha=0.9, stat="identity",fill="light blue") +
+      ggplot2::scale_x_continuous(name="Year", breaks = iyears)+
+      ggplot2::labs(y="No of studies") + ggplot2::ggtitle("Distribution of studies over time") +
+      ggplot2::theme_bw() +
+      ggplot2::theme(axis.line = ggplot2::element_line(colour = "black"),
+                     panel.background = ggplot2::element_blank(),
+                     plot.title = ggplot2::element_text(hjust = .5))
+
+    if(verbose) message("GenTimeTrend: Time trend plot created!")
+
 
   } else {
     message("GenTimeTrend: Year not found")
   } # if col exists
 
-  return(timemp)
- }
+  timemp
+}
