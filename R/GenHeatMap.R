48c719c90757c5da342e4059496a2117db253d65
mysteryduck <s.mitchell.2@research.gla.ac.uk>
2018-04-25 11:39:49 +0100

    GenHeatMap.R: Index data by column indices rather than colnames to avoid errors caused by unusual symbols

--- R/GenHeatMap.R
+++ R/GenHeatMap.R
@@ -3,8 +3,10 @@
 #' Feed in a dataset and get a heatmap in return
 #' Created For	  : ES Hackathon 2018
 #'
-#' @param idata Input data frame
-#' @param selcols Vector of strings with column names
+#' @param idata Input dataframe
+#' @param selcols Numeric vector of column indices
+#' @param verbose Comments
+#'
 #' @return Returns a heatmap object showing number of literature under different categories in user specified \code{selcols}
 #'
 #' @author Sanita Dhaubanjar 24 April 2018
@@ -19,9 +21,7 @@ GenHeatMap = function(idata, selcols, verbose = FALSE){
 
   # Count combination of vars --------
   seldata <- table(tmp)
-  seldata <- melt(seldata)
-  ind <- which(colnames(seldata) %in% "value")
-  colnames(seldata)[ind] <- "n"
+  seldata <- reshape2::melt(seldata)
 
   if(verbose) {
     message("GenHeatMap: Counting of var combination completed.")
@@ -29,19 +29,17 @@ GenHeatMap = function(idata, selcols, verbose = FALSE){
 
   # Plot Heatmap ------
   heatmp <- ggplot2::ggplot(seldata,
-                            ggplot2::aes_string(x=colnames(seldata[1]),
-                                                y=colnames(seldata[2]),
-                                                fill=colnames(seldata[3]))) +
+                            ggplot2::aes_string(x=seldata[,1], y=seldata[,2],
+                                                fill=seldata[,3])) +
     ggplot2::geom_raster(alpha=0.9) +
     ggplot2::geom_text(ggplot2::aes_string(label = colnames(seldata[3]))) +
     theme(axis.line = ggplot2::element_line(colour = "black"),
           panel.background = ggplot2::element_blank()) +
     ggplot2::scale_fill_gradient(low = "white", high = "light blue",
-                        name = "No of studies")
-  if (verbose) {
-    message("GenHeatMap: Heatmap created!")
-  }
-  return(heatmp)
+                                 name = "No of studies")
 
+  if(verbose) message("GenHeatMap: Heatmap created!")
+
+  heatmp
 }
 
