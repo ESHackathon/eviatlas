d9a4bb1952f24fe22fd24b2b1cde2446b2f9e17a
Sanita Dhaubanjar <sdhauban@Sanitas-MacBook-Pro.local>
2018-04-25 18:39:41 +0545

    Working function that takes colindx for region and creates bar plot #11

--- exploration/funct_locationtrend00.R
+++ exploration/funct_locationtrend00.R
@@ -2,51 +2,52 @@
 #' Simple R code to create bar plot with distribution of studies over the region from lat/long info
 #' Created For	  : ES Hackathon 2018
 #'
+#' @param idata Input dataframe
 #' @param idxlonlat Input data frame with two columns for longitude and latitude in degrees
-#' @param irgns Character variable with values "Country" or "Continent" to indicate regions used in bar plot
+#' @param selcolidx Index for column with location information
 #' @param verbose Logical variable (default=FALSE) for displaying messages on console or not
-#' @return Returns a bar plot object showing number of literature in different countries or continents as specified by user in \code{irgn}
+#' @return Returns a bar plot object showing number of literature in different locations
 #'
 #' @author Sanita Dhaubanjar
 #' @examples
-#' GenHeatMap(mpg, colnames(mpg[c(1,4)]))
 #'
 #=========================
 
-#GenLocationTrend = function(idata,idxlonlat, irgn, verbose = FALSE){
-#library(tidyverse)
-#library(ggplot2)
+GenLocationTrend = function(idata,idxlonlat, selcolidx, verbose = FALSE){
+library(tidyverse)
+library(ggplot2)
 
-# Inputs -------
-ifile <- file.path(getwd(),"data-raw","pilotdata.csv")
-data("pilotdata")
-idata<-pilotdata
-idxlonlat<- c(17,16)
-irgn<-"Country"
+# # Inputs -------
+# ifile <- file.path(getwd(),"data-raw","pilotdata.csv")
+# data("pilotdata")
+# idata<-pilotdata
+# idxlonlat<- c(17,16)
+# selcolidx<-10     #"Country"
 
-# Count combination of vars --------
-seldata <-mutate(idata, coords2country_continent(idata[idxlonlat]))
-seldata2 <-table(seldata[irgn])
+selcol<-colnames(idata[selcolidx])
 
-idata[selcols] %>%
-  add_count_(selcols) %>%
-  complete_(selcols, fill = list(n=0)) %>% distinct()
+# Count per locations --------
+seldata <- as.data.frame(table(idata[selcolidx]) )
+colnames(seldata)<-c(selcol, "n")
 
 # Plot bar chart
-iyears<- min(seldata$Year):max(seldata$Year)
 locmp<- ggplot(seldata, aes_string(x=colnames(seldata[1]),y=colnames(seldata[2]))) +
-  geom_bar(alpha=0.9, stat="identity",fill="light blue") +
+  geom_bar(alpha=0.9, stat="identity",fill="light green") +
   #geom_text(aes( label = colnames(seldata[2]))) + #fill = colnames(seldata[3]),
-  scale_x_continuous(name="Year", breaks = iyears)+
-  labs(y="No of studies")+ ggtitle("Distribution of studies over time")+
+  labs(y="No of studies")+ ggtitle("Distribution of studies over regions")+
   theme_bw()+
   theme(axis.line = element_line(colour = "black"),panel.background = element_blank(), plot.title = element_text(hjust = .5))
-timemp
+
+# Rotate xaxis label if too many
+if (nrow(seldata)>15){
+    locmp <- locmp + theme(axis.text.x=element_text(angle=45,hjust=0.95))
+    }
+
 if (verbose) {
-  message("GenTimeTrend: Time trend plot created!")
+  message("GenLocationTrend: Location trend plot created!")
 }
-timemp
 
-
-#return(heatmp)}
+locmp
+return(locmp)
+}
 
