68610475e1837357f84456070bb1658115d98a88
Andrew Feierman <andrew.feierman@gmail.com>
2018-04-28 20:47:11 -0400

    sys_map can now accept input for popup

--- R/sys_map.R
+++ R/sys_map.R
@@ -7,8 +7,14 @@
 #' @param longitude Specify which column contains the latitude in the `studies_data` dataframe.
 #' @export
 
-sys_map <- function(studies_data, latitude, longitude) {
-   leaflet::leaflet(studies_data) %>%
-   leaflet::addTiles() %>%
-   leaflet::addMarkers(lat = ~as.numeric(latitude), lng = ~as.numeric(longitude))
+sys_map <- function(studies_data, latitude, longitude, popup_user=NULL) {
+  if (!is.null(popup_user)) {
+    popups <- sapply(pilotdata[popup_user], as.character)
+  } else {popups <- NULL}
+  leaflet::leaflet(studies_data) %>%
+    leaflet::addTiles() %>%
+    leaflet::addMarkers(lat = ~as.numeric(latitude),
+                        lng = ~as.numeric(longitude),
+                        popup = ~paste(popups))
 }
+
