19d846c69313673ff9280433af121c676388662e
Andrew Feierman <andrew.feierman@gmail.com>
2019-07-22 21:41:46 +0200

    shapefiles will need to be done the old school way, without leaflet proxy (for now)

--- src/sys_map_shapefile.R
+++ src/sys_map_shapefile.R
@@ -1,11 +1,13 @@
-sys_map_shapefile <- function(shp) {
-  leaflet() %>%
+sys_map_shapefile <- function(shp, popups = "") {
+  
+  leaflet(shp) %>%
     addTiles(layerId = "atlas_basemap") %>% 
-    addFeatures(data = shp)
-  # , 
-  #               fillColor = "green", 
-  #               fillOpacity = 0.5, 
-  #               color = "black", 
-  #               weight = 2)
-  # 
+    addFeatures(data = shp, 
+                group = 'atlas_shapefile', 
+                fillColor = "green",
+                fillOpacity = 0.5,
+                color = "black",
+                weight = 2,
+                popup = ~popups)
+  
 }
