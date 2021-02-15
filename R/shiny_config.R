76b34cf9271e177546910745b12905470fdcf04c
Andrew Feierman <andrew.feierman@gmail.com>
2021-02-15 15:52:06 -0500

    change shiny config file to function

--- R/shiny_config.R
+++ R/shiny_config.R
@@ -1,7 +1,9 @@
 #' EviAtlas ShinyApp Configuration file
 #' @export
-shiny_config <- list()
-# maximum upload size 100 MB-- could be increased if proves problematic for users
-shiny_config$max_file_size_mb <- 100
+shiny_config <- function() {
+  config <- list()
+  # maximum upload size 100 MB-- could be increased if proves problematic for users
+  config$max_file_size_mb <- 100
 
-options(shiny.maxRequestSize = shiny_config$max_file_size_mb * 1024^2)
+  options(shiny.maxRequestSize = config$max_file_size_mb * 1024^2)
+}
